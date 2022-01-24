1b8bd65d6830bb3fb9f1ccede37cbef294299ffc
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-24 09:41:52 -0600

    Merged

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -29,6 +29,10 @@ public class Student {
         this.grades.add(grade);
     }
 
+    public void deleteGrade(int gradeToDelete) {
+        this.grades.remove(gradeToDelete);
+    }
+
     public double getGradeAverage() {
         double sum = 0;
         for (int grade: this.grades) {
@@ -37,40 +41,16 @@ public class Student {
         return sum / this.grades.size() ;
     }
 
-    public boolean updateGrade() {
-        Scanner scanner = new Scanner(System.in);
-        scanner.useDelimiter("\n");
+    public void updateGrade(int oldGrade, int newGrade) {
+        this.deleteGrade(oldGrade);
+        this.addGrade(newGrade);
+    }
 
-        //display list of grades
-        System.out.printf("Here are the grades for %s:%n", this.getName());
-        for (int i = 0; i < this.grades.size(); i++) {
-            System.out.println(this.grades.get(i));
-        }
-
-        //prompt user for which grade they would like to remove
-        System.out.print("What grade would you like to remove? ");
-        int userGradeToRemove = scanner.nextInt();
-
-        //prompt the user for what grade they would like to change it to
-        System.out.print("What grade would you like to change it to? ");
-        int userGradeToAdd = scanner.nextInt();
-
-        //loop through the Student's list of grades
-        for (int i = 0; i < this.grades.size(); i++) {
-            //if the input the user entered matches the value of the current iteration
-            //output the original grade (variable) was changed to (variable)
-            if(this.grades.get(i) == userGradeToRemove) {
-                this.grades.remove(this.grades.get(i));
-            }
-        }
-        this.addGrade(100);
-        System.out.println("Here is the new grades list:");
-
-        for (int i = 0; i < this.grades.size(); i++) {
-            System.out.println(this.grades.get(i));
-        }
-
-        return true; //was forced by IntelliJ to return something, even when return type was void in the method. This is why this method returns a boolean.
+    public void displayGrades() {
+        System.out.println("Here are the grades for " + this.getName() + ":");
+                for (int i = 0; i < this.grades.size(); i++) {
+                    System.out.println(this.grades.get(i));
+                }
     }
 
     public static void main(String[] args) {
@@ -82,8 +62,14 @@ public class Student {
         s1.addGrade(25);
         s1.addGrade(20);
 
-        System.out.println(s1.getGrades());
-        System.out.println(s1.getGradeAverage());
-        System.out.println(s1.updateGrade());
+
+        s1.displayGrades();
+//        s1.updateGrade(15, 100);//does not compile, fix it
+//        s1.displayGrades();
+
+//        System.out.println(s1.getGrades());
+//        System.out.println(s1.getGradeAverage());
+//        System.out.println(s1.updateGrade());
+
     }
 }//end class Student
